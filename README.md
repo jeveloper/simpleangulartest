54c13571dd096d38aab114a6ab5883286c1b54cc
Serge Bornow <serge.bornow@gmail.com>
2016-01-27 16:05:45 -0500

    added more information, added a nice loading feature to prevent double clickin

--- README.md
+++ README.md
@@ -20,3 +20,12 @@ Small fix to this generator:
 
  - gulp
 
+
+### features 
+ - demonstrating how to use resolve , when switching to a state, its best to load what you need prior to showing a view
+ - using a directive to handle tap/click and using NgCordova obtain geographical location (browser or device )
+ - for device there might be permissions settings that need to be present (not tested) 
+
+
+  - Upon loading the page, you'll see a quote pulled from api
+  - Click on I am located ... tap to refresh location  (this is the directive )
--- app/scripts/directives/somemodule/location.js
+++ app/scripts/directives/somemodule/location.js
@@ -2,26 +2,39 @@
 
 
 angular.module('Simpleangulartest.directives', ['ionic'])
-.directive('location', function ($ionicGesture,locationservices) {
+.directive('location', function ($ionicGesture,locationservices,$ionicLoading) {
     return {
     	restrict: 'EA',
         template: '<a href="#" > <strong>I am located at ... {{location}}, tap to refresh location</strong></a> ',
+        controller: function($scope){
+             $scope.show = function() {
+                $ionicLoading.show({
+                  template: 'Loading...'
+              });
+            };
+            $scope.hide = function(){
+                $ionicLoading.hide();
+            };
+        },
         link: function ($scope, $element, $attrs) {
             
         	var handleTap = function (e) {
         		//get new location
+                $scope.show();
                 console.log(" tapped or clicked");
                 
                 //call location services 
                 //data is lat and lon
                 locationservices.getmelocation().then(function(loc){
-                    $scope.location = loc;
+                    $scope.location = 'Latitude: '+loc.lat + ' , Longitude: '+ loc.lon ;
+                    $scope.hide();
                 }, function(err){
                     console.error('ouch');
                     console.error(err);
+                    $scope.location = "OH no!";
+                    $scope.hide();
                 });
-                // $scope.location = "Toronto";
-
+                
         	};
 
            
